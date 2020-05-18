2451e83725ce166649fe47d0ff5b261c0042f1f2
shownb <micoach@19cn.com>
2020-05-18 14:50:01 +0200

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,39 @@
 //ok
+basic.forever(function () {
+    if (ps2controller.button_pressed(ps2controller.PS2Button.Cross) > 0) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+    if (ps2controller.button_pressed(ps2controller.PS2Button.Square) > 0) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    }
+    if (ps2controller.button_pressed(ps2controller.PS2Button.Circle) > 0) {
+        basic.showLeds(`
+            . # # # .
+            # # . # #
+            # . . . #
+            # # . # #
+            . # # # .
+            `)
+    }
+    if (ps2controller.button_pressed(ps2controller.PS2Button.Triangle) > 0) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            . # . # .
+            # . . . #
+            # # # # #
+            `)
+    }
+})
